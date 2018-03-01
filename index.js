9cdf668e298dd00eafc3b8398651e95cff05057b
Lydia Barnes <lydia.barnes@addepar.com>
2018-03-01 15:44:40 -0800

    add new styles, ember-cli-sass, sass-import-once

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 'use strict';
 
 module.exports = {
-  name: 'addepar-toolbox'
+  name: 'addepar-toolbox',
+
+  included(app) {
+    app.options = app.options || {};
+    app.options.sassOptions = app.options.sassOptions || {};
+
+    const sass = require('node-sass');
+    const importOnce = require('node-sass-import-once');
+
+    if (!app.options.sassOptions.nodeSass) {
+
+      const sassRender = sass.render;
+
+      sass.render = function(options, callBack) {
+        options.importer = importOnce;
+        options.importOnce = {
+          index: false,
+          css: false,
+          bower: false
+        };
+
+        sassRender(options, callBack);
+      }
+    }
+
+    app.options.sassOptions.nodeSass = sass;
+  }
 };
