8b1e61142aeb87111652b49dd77c49c95d8dd640
Christopher Garrett <opensource@pzuraq.com>
2018-06-05 16:40:18 -0700

    [feat] Adds table and checkbox styles
    This PR add the following styles:
    
    * Styles for Checkbox elements (based on checkbox inputs)
    * Styles for Collapse toggle elements (based on checkbox inputs)
    * Base stylings for tables in general
    * Styles for Ember Table
    
    It also improves the build by extracting our autoprefixer logic out of
    Iverson and into this addon, so it can be shared between all
    apps/addons.

--- index.js
+++ index.js
@@ -2,45 +2,63 @@ module.exports = {
   name: '@addepar/style-toolbox',
 
   treeForStyles() {
-    const Funnel = require('broccoli-funnel');
-    const writeFile = require('broccoli-file-creator');
-    const mergeTrees = require('broccoli-merge-trees');
+    let Funnel = require('broccoli-funnel');
+    let writeFile = require('broccoli-file-creator');
+    let mergeTrees = require('broccoli-merge-trees');
 
     let stylesTree = new Funnel(`${this.root}/styles`, {
-      destDir: '@addepar/style-toolbox'
+      destDir: '@addepar/style-toolbox',
     });
 
-    let indexTree = writeFile(
-      '@addepar/style-toolbox.scss',
-      `@import './style-toolbox/index';`
-    );
+    let indexTree = writeFile('@addepar/style-toolbox.scss', "@import './style-toolbox/index';");
 
     return mergeTrees([stylesTree, indexTree]);
   },
 
   included(app) {
+    let sass = require('node-sass');
+    let importOnce = require('node-sass-import-once');
+    let autoprefixer = require('autoprefixer');
+
+    let defaultPostcssOptions = {
+      compile: {
+        enabled: false,
+      },
+      filter: {
+        enabled: true,
+        plugins: [
+          {
+            module: autoprefixer,
+            options: {
+              browsers: ['last 2 version', 'Safari >= 8'],
+            },
+          },
+        ],
+      },
+    };
+
     app.options = app.options || {};
     app.options.sassOptions = app.options.sassOptions || {};
-
-    const sass = require('node-sass');
-    const importOnce = require('node-sass-import-once');
+    app.options.postcssOptions = Object.assign(
+      app.options.postcssOptions || {},
+      defaultPostcssOptions
+    );
 
     if (!app.options.sassOptions.nodeSass) {
-
-      const sassRender = sass.render;
+      let sassRender = sass.render;
 
       sass.render = function(options, callBack) {
         options.importer = importOnce;
         options.importOnce = {
           index: false,
           css: false,
-          bower: false
+          bower: false,
         };
 
         sassRender(options, callBack);
-      }
+      };
     }
 
     app.options.sassOptions.nodeSass = sass;
-  }
+  },
 };
